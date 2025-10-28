1f87174ff3d49b4643863d82ba86630647caf57a
sauter-arch <peyton.a.sauter@gmail.com>
2025-10-28 13:29:55 -0500

    Add files via upload

---
+++ wages/code.js
@@ -0,0 +1,16 @@
+document.getElementById("btn").addEventListener("click", onclick)
+
+function onclick () {
+    let normal_hours = document.getElementById("nh").valueAsNumber
+    let normal_rate = document.getElementById("nr").valueAsNumber
+    let overtime_rate = document.getElementById("or").valueAsNumber
+    let hours_worked = document.getElementById("hw").valueAsNumber
+    let wage
+    if (hours_worked <= normal_hours) { // no overtime
+        wage = hours_worked * normal_rate
+    } else {
+        let overtime_hours = hours_worked - normal_hours
+        wage = normal_hours * normal_rate + overtime_hours * overtime_rate
+    }
+    document.getElementById("result").innerHTML = wage + " dollars"
+}
