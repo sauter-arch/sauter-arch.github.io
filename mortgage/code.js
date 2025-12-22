bfabc935e79ddc5a6ef4efc6b66d1ad44b4be293
sauter-arch <peyton.a.sauter@gmail.com>
2025-12-22 00:06:24 -0600

    Update code.js

--- mortgage/code.js
+++ mortgage/code.js
@@ -1,10 +1,11 @@
 document.getElementById("btn").addEventListener("click", onclick)
 
 function onclick() {
-    let P = document.getElementById("p").valueAsNumber
-    let n = 12*document.getElementById("years").valueAsNumber
-    let r = document.getElementById("rate").valueAsNumber / 100 / 12
-    let A = P * r * (1+r)**n / ((1+r)**n - 1)
-    let result = "The monthly payment is $" + A.toFixed(2) + "<br>The total payment is $" + (A*n).toFixed(2) + "<br>The total interest is $" + ((A*n)-P).toFixed(2)
+    let P = document.getElementById("p").valueAsNumber;
+    let n = 12*document.getElementById("years").valueAsNumber;
+    let r = document.getElementById("rate").valueAsNumber / 100 / 12;
+    let A = P * r * (1+r)**n / ((1+r)**n - 1);
+    let result = "The monthly payment is $" + A.toFixed(2) + "<br>The total payment is $" + (A*n).toFixed(2) + "<br>The total interest is $" + ((A*n)-P).toFixed(2);
     document.getElementById("result").innerHTML = result;
 }
+
